87ccc79e6e4826e62c3bab9ec970806a7224af57
Laurent Goderre <laurent.goderre@gmail.com>
2014-03-25 10:46:01 -0400

    Added the servo movement logic

--- Target.cpp
+++ Target.cpp
@@ -7,6 +7,12 @@
 #include <Adafruit_PWMServoDriver.h>
 #include "Target.h"
 
+//-------------------------------------
+// TODO: CALIBRATE THESE FOR SERVOS
+const int servoOnPosition = 150;
+const int servoOffPosition = 600;
+//-------------------------------------
+
 Target::Target(int targetNumber, Adafruit_PWMServoDriver pwm, int servoNumber) : number(targetNumber), pwm(pwm), servoNumber(servoNumber) {}
 
 Target::~Target(void){}
@@ -14,11 +20,13 @@ Target::~Target(void){}
 void Target::show()
 {
 	Serial.println("Showing target #" + (number + 1));
+	pwm.setPWM(servoNumber, 0, servoOnPosition);
 }
 
 void Target::hide()
 {
 	Serial.println("Hiding target #" + (number + 1));
+	pwm.setPWM(servoNumber, 0, servoOffPosition);
 }
 
 
